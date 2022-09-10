f87fbb70cb4447ab9807994eac3e7aca9bf99ff2
WonTaeJang <jwt0212@gmail.com>
2022-09-10 23:29:09 +0900

    router

---
+++ my-app/src/components/dangerButton.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import Button from "./button";
+
+class DangerButton extends Component {
+    render(){
+        return <button color="red"></button>
+    }
+}
+
+export default DangerButton;
